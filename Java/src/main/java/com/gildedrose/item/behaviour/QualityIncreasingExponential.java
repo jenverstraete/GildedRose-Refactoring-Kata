d15b0ffb802aa3f66bd874482ca677ef8d90a2bc
Jen Verstraete <jenverstraete@vanmarcke.be>
2019-10-28 08:58:17 +0100

    Removing commented method

--- Java/src/main/java/com/gildedrose/item/behaviour/QualityIncreasingExponential.java
+++ Java/src/main/java/com/gildedrose/item/behaviour/QualityIncreasingExponential.java
@@ -1,17 +1,6 @@
 package com.gildedrose.item.behaviour;
 
 public class QualityIncreasingExponential implements QualityBehaviour {
-    /*@Override
-    public void getChangeInQuality(@NotNull final ItemImpl item) {
-        item.incrementQuality();
-        if (item.sellIn < 11) {
-            item.incrementQuality();
-        }
-        if (item.sellIn < 6) {
-            item.incrementQuality();
-        }
-    }*/
-
     @Override
     public int getChangeInQuality(int sellDate) {
         return sellDate < 6 ? 3 : sellDate < 11 ? 2 : 1;
